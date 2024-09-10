48eea57748f71315ae47d8f9d995d30160278daf
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-09-10 20:49:16 +0100

    agent

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,7 +1,27 @@
+import { streamText, Message, LangChainAdapter  } from 'ai';
+import { DynamicTool, DynamicStructuredTool } from '@langchain/core/tools';
 import { ChatOpenAI } from '@langchain/openai';
-import { LangChainAdapter, Message } from 'ai';
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { AgentExecutor, createOpenAIToolsAgent, createOpenAIFunctionsAgent } from "langchain/agents";
+import { pull } from "langchain/hub";
+import { WikipediaQueryRun } from "@langchain/community/tools/wikipedia_query_run";
+import * as hub from "langchain/hub";
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+import * as z from "zod"
 import { AIMessage, HumanMessage } from '@langchain/core/messages';
 
+
+
+
+
+
+export const runtime = 'edge'
+
+
+
+
+//chatbot with langchain and with vercel ai sdk
+/*
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
@@ -27,38 +47,45 @@ export async function POST(req: Request) {
 
   return LangChainAdapter.toDataStreamResponse(stream);
 }
+ */
 
 
+
+//agent based chatbot with langchain and without vercel ai sdk
+
+ 
 
 /* 
-
-import { streamText, Message } from 'ai';
-import { DynamicTool, DynamicStructuredTool } from '@langchain/core/tools';
-import { ChatOpenAI } from '@langchain/openai';
-import type { ChatPromptTemplate } from "@langchain/core/prompts";
-import { AgentExecutor, createOpenAIToolsAgent, createOpenAIFunctionsAgent } from "langchain/agents";
-import { pull } from "langchain/hub";
-import { WikipediaQueryRun } from "@langchain/community/tools/wikipedia_query_run";
-import * as hub from "langchain/hub";
-
-import * as z from "zod"
-
-
-export const runtime = 'edge'
-
-
 export async function POST(req: Request) {
-  const {
-    messages,
-  }: {
-    messages: Message[];
-  } = await req.json();
+  const {messages} = await req.json();
+
+
+
+  // tools creation
+  
+  const searchTool = new TavilySearchResults();
+
+  const toolResult = await searchTool.invoke("what is two body problem?");
+
+  //console.log(toolResult);
+
+
+  const tools = [searchTool]
+  
 
   // initiaize the model
   const model = new ChatOpenAI({
-    model: 'gpt-4o', //gpt-3.5-turbo-0125
+    model: 'gpt-3.5-turbo', //gpt-3.5-turbo-0125
     temperature: 0,
   });
+
+
+  //respnse
+  const response = await model.invoke([new HumanMessage(messages)]);
+
+  //const modelWithTools = model.bindTools(tools);
+    return response;
+  
   // set up a wikipedia query tool for fetching relevant information
   const wikipediaQuery = new WikipediaQueryRun({
     topKResults: 1,
@@ -91,21 +118,29 @@ export async function POST(req: Request) {
       const data = await response.json();
       return data[cryptoName.toLowerCase()][vsCurrency.toLowerCase()].toString();
     },
-  });
+  }); 
 
   // 9. List all the tools that will be used by the agent during execution
-  const tools = [wikipediaQuery, foo, fetchCryptoPrice];
+  //const tools = [wikipediaQuery, foo, fetchCryptoPrice];
+
+  
 
 
-  const prompt =  await pull<ChatPromptTemplate>(
-    "hwchase17/openai-functions-agent"
-  );
+  const prompt = ChatPromptTemplate.fromMessages([
+  ["system", "You are a helpful assistant"],
+  ["user", `${messages}`],
+]);
 
   // 10. Initialize the agent executor, which will use the specified tools and model to process input
   const executor = await createOpenAIFunctionsAgent({
     model,
-    tool,
+    tools,
     prompt,
+  });
+
+  const agentExecutor = new AgentExecutor({
+    executor,
+    tools,
   });
 
   // 11. Extract the most recent input message from the array of messages
@@ -131,6 +166,87 @@ export async function POST(req: Request) {
 
   // 15. Send the created stream as a response to the client
   return new streamText.toDataStreamResponse(responseStream);
+ 
+} */
 
+
+
+
+
+/* //agent based chatbot with langchain and without vercel ai sdk and without tools
+
+
+export async function POST(req: Request) {
+  const {messages} = await req.json();
+
+
+
+  // tools creation
+  
+  const searchTool = new TavilySearchResults();
+
+  const toolResult = await searchTool.invoke("what is two body problem?");
+
+  //console.log(toolResult);
+
+
+  const tools = [searchTool]
+  
+
+  // initiaize the model
+  const model = new ChatOpenAI({
+    model: 'gpt-3.5-turbo', //gpt-3.5-turbo-0125
+    temperature: 0,
+  });
+
+
+  //respnse
+  const response = await model.invoke([new HumanMessage(messages)]);
+
+  //const modelWithTools = model.bindTools(tools);
+    return response;
+} */
+
+
+
+
+
+
+
+
+    //agent based chatbot with langchain and with vercel ai sdk and without tools
+
+
+export async function POST(req: Request) {
+  const {messages} = await req.json();
+
+
+
+  // tools creation
+  
+  const searchTool = new TavilySearchResults();
+
+  await searchTool.invoke("what is two body problem?");
+
+  //console.log(toolResult);
+
+
+  const tools = [searchTool]
+  
+
+  // initiaize the model
+  const model = new ChatOpenAI({
+    model: 'gpt-3.5-turbo', //gpt-3.5-turbo-0125
+    temperature: 0,
+  });
+
+
+  //respnse
+  const modelWithTools = model.bindTools(tools);
+  const response = await modelWithTools.invoke([
+    new HumanMessage(messages),
+  ]);
+
+  //const modelWithTools = model.bindTools(tools);
+    return response;
 }
- */
